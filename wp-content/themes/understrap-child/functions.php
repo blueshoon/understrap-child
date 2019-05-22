9b232aa86371ca6cabfb31fbec5363085c101282
Jamie Doherty <jamie@blueshoon.com>
2019-05-22 14:46:42 -0500

    include block registration for custom gutenberg blocks

--- wp-content/themes/understrap-child/functions.php
+++ wp-content/themes/understrap-child/functions.php
@@ -31,3 +31,6 @@ function add_child_theme_textdomain() {
     load_child_theme_textdomain( 'understrap-child', get_stylesheet_directory() . '/languages' );
 }
 add_action( 'after_setup_theme', 'add_child_theme_textdomain' );
+
+//blocks for gutenberg
+require_once('blocks/register-blocks.php');
